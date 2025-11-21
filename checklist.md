736a988503510e74cee154653504ed7594ced7e8
GabrielMM47 <gabrielmmendes@outlook.com>
2025-11-21 17:38:29 -0300

    Versão integrada do Blynk com sensor e persiana

---
+++ checklist.md
@@ -0,0 +1,10 @@
+Checklist
+
+1. Controle do Status quando utilizarmos botões físicos
+2. Modelagem 3D 
+3. Modelagem da placa
+4. Modos de iluminação + Integração com Blynk
+5. Integração do Blynk com as luzes
+6. Grafana + PostgreSQL
+7. Juntar código principal de integração da persiana, luzes e Blynk com o código do Display
+8. 
--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,25 @@
 #include <MQTT.h>
 #include <RCSwitch.h>
 #include <GFButton.h>
+#include <Preferences.h>
 
 //MUDAR DEPOIS
 #define RF_PORT 2
 #define B1_PORT 5
 #define B2_PORT 6
 #define B3_PORT 7
+#define LDR_PIN 18
 
+//Configurações do MQTT e Blynk
 String MQTT_HOST = "ny3.blynk.cloud";
 int MQTT_PORT = 8883;              // TLS
 String MQTT_USER = "device";             // fixo
 String BLYNK_AUTH_TOKEN = "xLNpnJ0e6f3JaeamhnZuKbRhxakC9lhK";   // password do MQTT
 
+// Objetos globais
 WiFiClientSecure conexaoSegura;
 MQTTClient mqtt(1000);
-
-int sensorLuz = 18; // Pino 10
-unsigned long instanteAnterior = 0;
-int estadoPersiana = 0;
+Preferences preferencias;
 
 //Botões
 GFButton button1(B1_PORT);
@@ -35,6 +36,24 @@ const unsigned long UP_CODE = 2448754;
 const unsigned long DOWN_CODE = 2448756;
 const unsigned long STOP_CODE = 2448760;
 const int BITS = 24;
+
+//Variaveis de controle integrado da persiana com a luz
+int fechar = 0;
+int luminosidadeAtual = 0;
+float metaNum = 40;
+int statusPersiana = 0;
+int novoStatus = -1;
+int comandoPersiana = 0;
+int statusPersianaBlynk = 0;
+int autoMode = 0; // 0 - manual, 1 - automático
+
+//Variaveis de tempo
+unsigned long instanteAnterior = 0; //Instante para checagem do sensor de luz
+unsigned long instanteAnterior2 = 0; //Instante para checagem da persiana
+unsigned long instanteAnteriorAjuste = 0;
+unsigned long periodoDeChecagem = 10; //em Segundos
+
+
 
 //Handlers dos botões
 void upButtonPressed(GFButton& button) {
@@ -81,8 +100,16 @@ void reconectarMQTT() {
     }
 }
 
+void ajustePersiana (const unsigned long code, int tempoDeMovimento) {
+    unsigned long instanteInicioAjuste = millis();
+    mySwitch.send(code, BITS);
+    while (millis() < instanteInicioAjuste + tempoDeMovimento) {
+    }//DELAY
+    mySwitch.send(STOP_CODE, BITS);
+}
+
 void recebeuMensagem(String topico, String conteudo) {
-    Serial.println(topico + ": " + conteudo);
+    Serial.println("\n" + topico + ": " + conteudo);
     if (topico.endsWith(String("downlink/ds/LED"))) {
         int valor = conteudo.toInt();
         Serial.println("Valor recebido: " + String(valor));
@@ -93,17 +120,56 @@ void recebeuMensagem(String topico, String conteudo) {
             rgbLedWrite(RGB_BUILTIN, 0, 0, 0);
         }
     }
+
     if (topico.endsWith(String("downlink/ds/Persiana"))) {
-        int estadoPersiana = conteudo.toInt();
-        Serial.println("Valor recebido: " + String(estadoPersiana));
-        if (estadoPersiana == 0) {
+        int comandoPersiana = conteudo.toInt();
+        Serial.println("Valor recebido: " + String(comandoPersiana));
+        if (comandoPersiana == 0) {
             mySwitch.send(STOP_CODE, BITS);
         }
-        else if (estadoPersiana == 1) {
+        else if (comandoPersiana == 1) {
             mySwitch.send(UP_CODE, BITS);
         }
-        else if (estadoPersiana == 2) {
+        else if (comandoPersiana == 2) {
             mySwitch.send(DOWN_CODE, BITS);
+        }
+    }
+
+    if (topico.endsWith(String("downlink/ds/lightMeta"))) {
+        metaNum = conteudo.toInt();
+        preferencias.putInt("metaLum", metaNum);
+        Serial.println("Nova meta de luminosidade: " + String(metaNum) + "%");
+    }
+
+    if (topico.endsWith(String("downlink/ds/blindStatus"))) {
+        statusPersianaBlynk = conteudo.toInt();
+        preferencias.putInt("statusPer", statusPersianaBlynk);
+        Serial.println("Novo status da persiana via Blynk: " + String(statusPersianaBlynk) + "%");
+        
+        int diferenca = statusPersianaBlynk - statusPersiana;
+        int tempoDeEspera = abs(diferenca) * 300;
+        if (diferenca > 0) {//descer persiana
+            ajustePersiana(DOWN_CODE, tempoDeEspera);
+            statusPersiana = statusPersianaBlynk;
+        }
+        else if (diferenca < 0) {
+            ajustePersiana(UP_CODE, tempoDeEspera);
+            statusPersiana = statusPersianaBlynk;
+        }
+        else {
+            Serial.println("Persiana já está na posição desejada.");
+        }
+    }
+
+    if (topico.endsWith(String("downlink/ds/autoMode"))) {
+        autoMode = conteudo.toInt();
+        preferencias.putInt("autoMode", autoMode);
+        Serial.println("Novo modo de operação recebido: " + String(autoMode));
+        if (autoMode == 1) {
+            Serial.println("Modo automático ativado.\n");
+        }
+        else {
+            Serial.println("Modo automático desativado.\n");
         }
     }
 }
@@ -126,6 +192,10 @@ void setup() {
     button2.setPressHandler(downButtonPressed);
     button3.setPressHandler(stopButtonPressed);
 
+    preferencias.begin("dadosPersiana");
+    statusPersiana = preferencias.getInt("statusPer", 0);
+    metaNum = preferencias.getInt("metaLum", -1);
+    autoMode = preferencias.getInt("autoMode", 0);
 }
 
 void loop() {
@@ -137,14 +207,75 @@ void loop() {
     button3.process();
 
     unsigned long instanteAtual = millis();
-    if (instanteAtual > instanteAnterior + 5000) {  
-        int leitura = analogRead(sensorLuz);
-        int porcentagemLuz = map(leitura, 0, 4095, 0, 100);
-        Serial.print("Luz: "); Serial.print(porcentagemLuz); Serial.println("%");
-        String conteudo = String(porcentagemLuz);
+    if (instanteAtual > instanteAnterior + 3000) {  
+        int leitura = analogRead(LDR_PIN);
+        luminosidadeAtual = map(leitura, 0, 4095, 0, 100);
+        Serial.print("Luminosidade atual: " + String(luminosidadeAtual) + "%\n");
+        
+        //Envio da Luminosidade via MQTT para o Blynk
+        String conteudo = String(luminosidadeAtual);
         mqtt.publish("ds/LightSensor", conteudo);
 
+        if (metaNum == -1){//Não tem meta 
+            Serial.println("Sem meta!");
+        } 
 
         instanteAnterior = instanteAtual;
+    }
+
+     if ((instanteAtual > instanteAnterior2 + periodoDeChecagem*1000) && (metaNum != -1) && (autoMode == 1)){
+        Serial.println("\nVerificando necessidade de ajuste na persiana...");
+        Serial.println("Status atual da persiana: " + String(statusPersiana) + "%");
+        Serial.println("Luminosidade atual: " + String(luminosidadeAtual) + "%");
+        Serial.println("Meta de luminosidade: " + String(metaNum) + "%\n");
+        
+        if ((luminosidadeAtual < (metaNum + 5)) && (luminosidadeAtual > (metaNum -5))){// Estamos na meta!
+            //nao faz nada...
+            Serial.println("\nEstamos na meta!");
+        }
+
+        else { // se está fora do treshold
+
+            if (( (statusPersiana == 0) && (metaNum > luminosidadeAtual)) || ( (statusPersiana == 100) && (metaNum < luminosidadeAtual) ) ){//Nesse caso não da mais para fazer nada
+                Serial.println("\nInfelizmente nao sera possivel atingir a meta, a persiana já está no limite. :(");
+            }
+            else{
+                if (luminosidadeAtual < metaNum){//Aqui devemos subir a persiana.
+                    //Se 30 segundos é 100% da persiana, queremos subir 5%. Logo precisamos subir a persiana por 1,5 segundos
+                    novoStatus = statusPersiana - 5;
+                    if (novoStatus < 0){
+                        statusPersiana = 0;
+                    }
+                    else{
+                        statusPersiana = novoStatus;
+                        // mySwitch.send(UP_CODE, BITS);
+                        // delay(2000);//dando 500 milisegundos de folga - testar melhor...
+                        // mySwitch.send(STOP_CODE,BITS);
+                        ajustePersiana(UP_CODE, 2000);
+                    }
+                    mqtt.publish("ds/blindStatus", String(statusPersiana));
+                    preferencias.putInt("statusPer", statusPersiana);
+                }
+                
+                else{
+                    //Se 30 segundos é 100% da persiana, queremos descer 5%. Logo precisamos subir a persiana por 1,5 segundos
+                    novoStatus = statusPersiana + 5;
+                    if (novoStatus > 100){
+                        statusPersiana = 100;
+                    }
+                    else{
+                        statusPersiana = novoStatus;
+                        // mySwitch.send(DOWN_CODE, BITS);
+                        // delay(2000); //dando 500 milisegundos de folga - testar melhor...
+                        // mySwitch.send(STOP_CODE,BITS);
+                        ajustePersiana(DOWN_CODE, 2000);
+                    }   
+                    mqtt.publish("ds/blindStatus", String(statusPersiana));
+                    preferencias.putInt("statusPer", statusPersiana);
+                }
+            }
+            Serial.println("Novo status da persiana: " + String(statusPersiana) + "%\n");
+        }
+        instanteAnterior2 = instanteAtual;
     }
 }
